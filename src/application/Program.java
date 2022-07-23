7638447af497e51a9550ed515c9142439417f00d
Thiago Mota <thiagomota99@outlook.com>
2022-07-23 13:14:05 -0300

    Criando a classe Departamento

---
+++ src/application/Program.java
@@ -0,0 +1,12 @@
+package application;
+
+import model.entities.Departamento;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Departamento departamento = new Departamento(1, "Books");
+		System.out.println(departamento);
+	}
+
+}
