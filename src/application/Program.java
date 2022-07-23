65080e45196a6610c1b20f0f0a3a451e1e4a16df
Thiago Mota <thiagomota99@outlook.com>
2022-07-23 13:23:37 -0300

    Criando classe Vendedor

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Departamento departamento = new Departamento(1, "Books");
-		System.out.println(departamento);
+		
+		Vendedor vendedor = new Vendedor(21, "Bob", "bob@gmail.com", new Date(), 3000.0, departamento);
+		System.out.println(vendedor);
 	}
 
 }
